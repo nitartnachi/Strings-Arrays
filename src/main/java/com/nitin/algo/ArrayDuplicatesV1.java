147c9ebc47b026de413706ed52194c44dd93c8e9
Nitin Sharma <nsharma78@gmail.com>
2017-10-23 11:37:27 -0700

    Anagrams and Duplicates in array

--- src/main/java/com/nitin/algo/ArrayDuplicatesV1.java
+++ src/main/java/com/nitin/algo/ArrayDuplicatesV1.java
@@ -1,10 +1,40 @@
 /*
  Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, 
  and it should return false if every element is distinct.
+ * ----------------------------------------------------------------------------------------------------
+ * BIG O Details: time O(arr.length) -- space O(1) the key is in place with just one pass of the array, will not work for negative numbers in the array
  */
 
 package com.nitin.algo;
 
 public class ArrayDuplicatesV1 {
+	
+	private static boolean hasDuplicates(int[] arr) {
+
+		// handle error conditions and corner case
+		if(arr == null || arr.length < 2)
+			return false;
+		
+		int len = arr.length;
+		
+		for(int i = 0; i < len; ++i) {
+			int curr = Math.abs(arr[i]);
+			if(curr >= arr.length)
+				continue;
+			if(arr[curr] < 0)
+				return true;
+			arr[curr] = 0 - arr[curr];
+		}
+		return false;
+	}
+
+	// test harness
+	public static void main(String[] args) {
+		
+		int[] arr = new int[] {1,3,4,5,7, 8,9, 10, 1};
+		//int[] arr = new int[] {};
+		
+		System.out.println("The input array has duplicates: " + hasDuplicates(arr));
+	}
 
 }
--- src/main/java/com/nitin/algo/BinaryAddition.java
+++ src/main/java/com/nitin/algo/BinaryAddition.java
@@ -1,11 +1,42 @@
 /*
  Given two binary strings, return their sum (also a binary string).
-
-For example, a = "11", b = "1", the return is "100".
+ For example, a = "11", b = "1", the return is "100".
  */
 
 package com.nitin.algo;
 
 public class BinaryAddition {
+	
+	private static String binarySum(String s1, String s2) {
+
+		// handle error conditions and corner case
+		if(s1 == null || s1.isEmpty()) {
+			if(s2 == null)
+				return null;
+			else {
+				if(s2.isEmpty())
+					return null;
+				else
+					return s2;
+			}
+		}
+		
+		if(s2 == null || s2.isEmpty()) {
+			if(s1.isEmpty())
+				return null;
+			else
+				return s1;
+		}
+		return null;
+	}
+
+	// test harness
+	public static void main(String[] args) {
+		
+		String s1 = "11";
+		String s2 = "1";
+		
+		System.out.println("The binary sum of the given numberts is : " + binarySum(s1, s2));
+	}
 
 }
