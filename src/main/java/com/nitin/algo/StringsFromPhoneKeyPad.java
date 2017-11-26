ad027c29c1cb2c5bd67a4dea9ccfae2b32a946ee
Nitin Sharma <nsharma78@gmail.com>
2017-11-26 12:49:10 -0800

    StringsFromPhoneKeyPad implemented

--- src/main/java/com/nitin/algo/StringsFromPhoneKeyPad.java
+++ src/main/java/com/nitin/algo/StringsFromPhoneKeyPad.java
@@ -7,20 +7,37 @@ Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 
 package com.nitin.algo;
 
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class StringsFromPhoneKeyPad {
 	
 	public static void main(String[] args) {
-		String input = "23";
+		String input = "123456789";
 		List<String> list = wordsFromPhoneKeyPad(input);
-		System.out.println("The string for count and say is: " + list.toString());
+		System.out.println(list.size() + " The words formed from given digits are: " + list.toString());
 	}
 
-	private static List<String> wordsFromPhoneKeyPad(String input) {
-		// TODO Auto-generated method stub
-		return Collections.emptyList();
-	}
+	private static List<String> wordsFromPhoneKeyPad(String digits) {
+        String[] letters = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        LinkedList<String> list = new LinkedList<>();
+        list.add("");
+        
+        for (int i = 0; i < digits.length(); i++) {
+        	
+            int index = digits.charAt(i) - '2';
+            if(index < 0 || index > letters.length)
+            		continue;
+            int size = list.size();
+            
+            for (int k = 0; k < size; k++) {
+                String tmp = list.remove();
+                for (int j = 0; j < letters[index].length(); j++)
+                	list.add(tmp + letters[index].charAt(j));
+            }
+            
+        }
+        return list;
+    }
 
 }
