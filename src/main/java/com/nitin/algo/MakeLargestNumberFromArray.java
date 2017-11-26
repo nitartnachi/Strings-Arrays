b901587a6e73906c19debafcb2a363a0d9eb5f85
Nitin Sharma <nsharma78@gmail.com>
2017-11-25 16:01:00 -0800

    MakeLargestNumberFromArray implemented

---
+++ src/main/java/com/nitin/algo/MakeLargestNumberFromArray.java
@@ -0,0 +1,42 @@
+/*
+ Given a list of non negative integers, arrange them such that they form the largest number.
+For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330. 
+(Note: The result may be very large, so you need to return a string instead of an integer.)
+ */
+
+package com.nitin.algo;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MakeLargestNumberFromArray {
+	
+	public static void main(String[] args) {
+		int[] arr = {3,30,34,5,9};
+		System.out.println("The largest number made from the array is: " + largestNumber(arr));
+	}
+	
+	private static String largestNumber(int[] arr) {
+		if(arr == null || arr.length == 0)
+			return null;
+		
+		String[] strArray = new String[arr.length];
+		for(int i = 0; i < arr.length; i++)
+			strArray[i] = String.valueOf(arr[i]);
+		
+		Arrays.sort(strArray, new Comparator<String>() {
+			public int compare(String a, String b){
+	            return (b+a).compareTo(a+b);
+	        }
+		});
+		
+		System.out.println(Arrays.toString(strArray));
+		StringBuilder sb = new StringBuilder();
+		for(String str : strArray)
+			sb.append(str);
+		if(sb.charAt(0) == '0')
+			sb.deleteCharAt(0);
+		return sb.toString();
+	}
+
+}
