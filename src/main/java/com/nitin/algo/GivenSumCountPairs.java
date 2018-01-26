f30a0f0e5cb5e64299e47b3c0bc8b6de34c1308f
Nitin Sharma <nsharma78@gmail.com>
2018-01-25 17:55:47 -0800

    GivenSumCountPairs implemented

---
+++ src/main/java/com/nitin/algo/GivenSumCountPairs.java
@@ -0,0 +1,40 @@
+package com.nitin.algo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GivenSumCountPairs {
+	
+	public static void main(String[] args) {
+		int arr[] = {1, 5, 7, -1, 5} ;
+        int sum = 6;
+        System.out.println("Count of pairs is " + getPairsCount(arr, arr.length,sum));
+         
+    }
+
+	private static int getPairsCount(int[] arr, int n, int sum) {
+		Map<Integer, Integer> hm = new HashMap<>();
+		 
+        // Store counts of all elements in map hm
+        for (int i = 0; i < n; i++) {
+            if(!hm.containsKey(arr[i]))
+                hm.put(arr[i], 0);
+            hm.put(arr[i], hm.get(arr[i]) + 1);
+        }
+        int twiceCount = 0;
+ 
+        // iterate through each element and increment the
+        // count (Notice that every pair is counted twice)
+        for (int i = 0; i < n; i++) {
+            if(hm.get(sum - arr[i]) != null)
+            		twiceCount += hm.get(sum - arr[i]);
+ 
+            if (sum-arr[i] == arr[i])
+            		twiceCount--;
+        }
+ 
+        // return the half of twice_count
+        return twiceCount/2;
+	}
+
+}
